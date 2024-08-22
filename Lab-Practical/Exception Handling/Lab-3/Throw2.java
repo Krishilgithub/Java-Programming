0033d403eb944203f9ab0d0477c6d7648f100a17
Krishilgithub <100427701+Krishilgithub@users.noreply.github.com>
2024-08-22 12:50:09 +0530

    Make seprate folders

---
+++ Lab-Practical/Exception Handling/Lab-3/Throw2.java
@@ -0,0 +1,7 @@
+public class Throw2 {
+    public static void main(String[] args) {
+        System.out.println("User Defined Exception");
+        throw new ArithmeticException("Charusat/by zero");
+        // System.out.println("Rest of the code");
+    }
+}
